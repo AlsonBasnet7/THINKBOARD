f78e8aed616db6a6ffa17e3e191a12d82bbbdd6f
AlsonBasnet07 <basnetalson121@gmail.com>
2026-05-24 23:22:22 +0545

    Add initial implementation of the add function

---
+++ 01/function.js
@@ -0,0 +1,6 @@
+function add(a,b){
+    return a+b
+}
+var number1=Number(prompt('Enter the first number'))
+var number2=Number(prompt("Enter the second number"))
+document.write(add(number1,number2));
